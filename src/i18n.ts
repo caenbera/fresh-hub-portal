3e04c29c4132bf1272ad40404e2840f90dcfaead
Carlos Bermeo <caenbera@gmail.com>
2026-01-18 13:35:54 +0000

    mirando yo como estaba el codigo que te dí de:"src/i18n.ts"; me puse yo

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,29 @@
 import {getRequestConfig} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import { locales } from './i18n-config';
+
+async function loadMessages(locale: string) {
+  switch (locale) {
+    case 'en':
+      return {
+        Dashboard: (await import('../messages/en/Dashboard.json')).default,
+        Auth: (await import('../messages/en/Auth.json')).default,
+      };
+    case 'es':
+      return {
+        Dashboard: (await import('../messages/es/Dashboard.json')).default,
+        Auth: (await import('../messages/es/Auth.json')).default,
+      };
+    default:
+      return {};
+  }
+}
  
 export default getRequestConfig(async ({locale}) => {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale as any)) notFound();
  
   return {
-    messages: (await import(`../messages/${locale}.json`)).default
+    messages: await loadMessages(locale)
   };
 });
