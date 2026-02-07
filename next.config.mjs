fb095646135a57313729521bb39fc8c372d8858e
Carlos Bermeo <caenbera@gmail.com>
2026-02-07 16:04:14 +0000

    Try fixing this error: `Runtime Error: Error: Couldn't find next-intl co

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,36 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: false,
-  webpack: (config) => {
-    config.externals = [...config.externals, 'pwa-asset-generator'];
-    return config;
-  }
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.postimg.cc',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            },
+            {
+                protocol: 'https',
+                hostname: 'via.placeholder.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 's.groovevideo.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ui-avatars.com',
+            },
+        ],
+    },
 };
 
-export default nextConfig;
+const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
+ 
+module.exports = withNextIntl(nextConfig);
